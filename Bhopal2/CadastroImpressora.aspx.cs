7f9eef10a83cdbdb3473d864fd334bc2ae4489a1
fabianohkd <fabianoconrado@gmail.com>
2017-01-13 13:39:34 -0200

    Corrigindo erro na tela de cadastro de impressoras

--- Bhopal2/CadastroImpressora.aspx.cs
+++ Bhopal2/CadastroImpressora.aspx.cs
@@ -17,24 +17,24 @@ namespace Bhopal2
                 //carregando dados no dropDownList
                 //Fornecedor
                 var f = new FornecedorDAO();
-                var IMPRESSORAS = f.getAll();
+                var impressoras = f.getAll();
                 ddlFornecedor.DataValueField = "Id";
                 ddlFornecedor.DataTextField = "Nome";
-                ddlFornecedor.DataSource = IMPRESSORAS;
+                ddlFornecedor.DataSource = impressoras;
                 ddlFornecedor.DataBind();
                 //Modelo
                 var m = new ModeloDAO();
                 var modelos = m.getAll();
                 ddlModelo.DataValueField = "Id";
                 ddlModelo.DataTextField = "Nome";
-                ddlModelo.DataSource = IMPRESSORAS;
+                ddlModelo.DataSource = modelos;
                 ddlModelo.DataBind();
                 //Departamento
                 var d = new DepartamentoDAO();
                 var departamentos = d.getAll();
                 ddlDepartamento.DataValueField = "Id";
                 ddlDepartamento.DataTextField = "Nome";
-                ddlDepartamento.DataSource = IMPRESSORAS;
+                ddlDepartamento.DataSource = departamentos;
                 ddlDepartamento.DataBind();
             }
         }
